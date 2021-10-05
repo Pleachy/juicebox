7faec2b1ddbda566dca1698d892178fa2de5c5e9
Pleachy <daniel.golub@gmail.com>
2021-10-05 16:29:28 -0700

    api

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const postsRouter = express.Router();
 
-const {getAllPosts} = require('../db');
+const {getAllPosts, createPost} = require('../db');
+const {requireUser} = require('./utils');
 
 postsRouter.use((req, res, next) => {
     next();
@@ -13,6 +14,42 @@ postsRouter.get('/' , async (req, res, next) => {
     res.send({
         posts: posts
     })
+})
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+    const {title, content, tags = ""} = req.body;
+
+    const tagArr = tags.trim().split(/\s+/);
+    const postData = {};
+
+    //only send tags if there are some to send
+    if (tagArr.length) {
+        postData.tags = tagArr;
+    }
+
+    try {
+        postData.authorId = req.user.id;
+        postData.title = title;
+        postData.content = content;
+        postData.tags = tagArr;
+
+        const post = await createPost(postData);
+
+        if (post) {
+            res.send({post});
+        } else {
+            next({
+                name: 'PostCreationerror',
+                message: 'Invalid fields for post'
+            })
+        }
+
+    } catch ({name, message}) {
+        next ({name, message})
+    }
+
+
+    res.send({message: "under constructsion"});
 })
 
 module.exports = postsRouter;
